eeb0f2dd3e8ddb26a2fea3e405cd8512e89d8b7b
Quentin Lurkin <qlurkin@gmail.com>
2026-04-04 23:12:19 +0200

    Packagify

---
+++ src/stuge/__init__.py
@@ -0,0 +1,13 @@
+from .stuge import run, turtle, delta_time, set_loop, keypresses, size, quit
+from turtle import clear
+
+__all__ = [
+    "run",
+    "turtle",
+    "delta_time",
+    "set_loop",
+    "keypresses",
+    "size",
+    "quit",
+    "clear",
+]
--- src/stuge/stuge.py
+++ src/stuge/stuge.py
@@ -1,9 +1,24 @@
 from __future__ import annotations
+import tkinter
+from os import environ
+from pathlib import Path
+from sys import base_prefix
+import platform
 import turtle
 from dataclasses import dataclass, field
 from typing import Any, Callable
 import sys
 from time import perf_counter
+
+# Hack to make uv venv find Tcl
+if not ("TCL_LIBRARY" in environ and "TK_LIBRARY" in environ):
+    try:
+        tkinter.Tk()
+    except tkinter.TclError:
+        tk_dir = "tcl" if platform.system() == "Windows" else "lib"
+        tk_path = Path(base_prefix) / tk_dir
+        environ["TCL_LIBRARY"] = str(next(tk_path.glob("tcl8.*")))
+        environ["TK_LIBRARY"] = str(next(tk_path.glob("tk8.*")))
 
 KEYS = [
     "space",
